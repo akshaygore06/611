304bceb6a7f117c1c7693107b830d5260f598d87
Akshay Gore <agore1@mail.csuchico.edu>
2016-04-23 20:19:31 -0600

    communication original server socket code

--- test_prg.cpp
+++ test_prg.cpp
@@ -798,115 +798,87 @@ void create_server_daemon()
 	// 	sleep(2);
 	// }
 
-/////////////////////////////////////SOCKET/////////////////////////////////////
 
-int sockfd; //file descriptor for the socket
-int status; //for error checking
+	int sockfd; //file descriptor for the socket
+	int status; //for error checking
 
-//change this # between 2000-65k before using
-const char* portno="42424";
-struct addrinfo hints;
-memset(&hints, 0, sizeof(hints)); //zero out everything in structure
-hints.ai_family = AF_UNSPEC; //don't care. Either IPv4 or IPv6
-hints.ai_socktype=SOCK_STREAM; // TCP stream sockets
-hints.ai_flags=AI_PASSIVE; //file in the IP of the server for me
+	//change this # between 2000-65k before using
+	const char* portno="42424";
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints)); //zero out everything in structure
+	hints.ai_family = AF_UNSPEC; //don't care. Either IPv4 or IPv6
+	hints.ai_socktype=SOCK_STREAM; // TCP stream sockets
+	hints.ai_flags=AI_PASSIVE; //file in the IP of the server for me
 
-struct addrinfo *servinfo;
-if((status=getaddrinfo(NULL, portno, &hints, &servinfo))==-1)
-{
-	fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-	exit(1);
-}
-sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	struct addrinfo *servinfo;
+	if((status=getaddrinfo(NULL, portno, &hints, &servinfo))==-1)
+	{
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+		exit(1);
+	}
+	sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
-/*avoid "Address already in use" error*/
-int yes=1;
-if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1)
-{
-	perror("setsockopt");
-	exit(1);
-}
+	/*avoid "Address already in use" error*/
+	int yes=1;
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1)
+	{
+		perror("setsockopt");
+		exit(1);
+	}
 
-//We need to "bind" the socket to the port number so that the kernel
-//can match an incoming packet on a port to the proper process
-if((status=bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))==-1)
-{
-	perror("bind");
-	exit(1);
-}
-//when done, release dynamically allocated memory
-freeaddrinfo(servinfo);
+	//We need to "bind" the socket to the port number so that the kernel
+	//can match an incoming packet on a port to the proper process
+	if((status=bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))==-1)
+	{
+		perror("bind");
+		exit(1);
+	}
+	//when done, release dynamically allocated memory
+	freeaddrinfo(servinfo);
 
-if(listen(sockfd,1)==-1)
-{
-	perror("listen");
-	exit(1);
-}
+	if(listen(sockfd,1)==-1)
+	{
+		perror("listen");
+		exit(1);
+	}
 
-		write(debugFD, "Blocking, waiting for client to connect\n", sizeof("Blocking, waiting for client to connect\n"));
-  //printf("Blocking, waiting for client to connect\n");
+	printf("Blocking, waiting for client to connect\n");
 
+	struct sockaddr_in client_addr;
+	socklen_t clientSize=sizeof(client_addr);
+	int new_sockfd;
+	if((new_sockfd=accept(sockfd, (struct sockaddr*) &client_addr, &clientSize))==-1)
+	{
+		perror("accept");
+		exit(1);
+	}
 
-  struct sockaddr_in client_addr;
-  socklen_t clientSize=sizeof(client_addr);
-  int new_sockfd;
-	//cerr << "cerr Accepting\n";
-  if((new_sockfd=accept(sockfd, (struct sockaddr*) &client_addr, &clientSize))==-1)
-  {
-    perror("accept");
-    exit(1);
-  }
-write(debugFD,"Accepted\n",sizeof("Accepted\n")); /// writting to pipe
-  //read & write to the socket
-  char buffer[100];
- memset(buffer,0,100);
-  int n;
-  if((n=read(new_sockfd, buffer, 99))==-1)
-  {
-    perror("read is failing");
-    printf("n=%d\n", n);
-  }
+	//read & write to the socket
+	char buffer[100];
+	memset(buffer,0,100);
+	int n;
+	if((n=READ(new_sockfd, buffer, 99))==-1)
+	{
+		perror("read is failing");
+		printf("n=%d\n", n);
+	}
 
-//	sleep(10);
-			  // ///writing to the socket
-				// if(write(new_sockfd, &rows, sizeof(int)) == -1)
-				// 	{
-				// 		perror("writing error");
-				// 	}
-				// if(write(new_sockfd, &cols, sizeof(int)) == -1)
-				// 	{
-				// 		perror("writing error");
-				// 	}
-				//
-				// if(write(new_sockfd, local_mapcopy, rows*cols) ==-1)
-				// 	{
-				// 		perror("writing error");
-				// 	}
-				// 	while(1)
-				// 	{
-				// 	}
+	// char arr[11];
+	// memset(arr, 0, 11);
+	// READ(new_sockfd, arr, 3);
+	// cout << "first read=" << arr << endl;
+	// memset(arr, 0, 11);
+	// READ(new_sockfd, arr, 4);
+	// cout << "second read=" << arr << endl;
+	// memset(arr, 0, 11);
+	// READ(new_sockfd, arr, 3);
+	// cout << "third read=" << arr << endl;
 
-  // char arr[11];
-  // memset(arr, 0, 11);
-  // READ(new_sockfd, arr, 3);
-  // cout << "first read=" << arr << endl;
-  // memset(arr, 0, 11);
-  // READ(new_sockfd, arr, 4);
-  // cout << "second read=" << arr << endl;
-  // memset(arr, 0, 11);
-  // READ(new_sockfd, arr, 3);
-  // cout << "third read=" << arr << endl;
+	printf("The client said: %s\n", buffer);
 
-//  printf("The client said: %s\n", buffer);
-
-//std::cerr << "/* The client said: \n"<< buffer << std::endl;
-  WRITE(debugFD,"before wrintg to socket..!!\n",sizeof("before wrintg to socket..!!\n")); //  debuging pipe
-	const char* message="These are the times that try men's souls.\n";
-  //cerr << "\nwriting";
-	write(new_sockfd, message, strlen(message));
-  close(new_sockfd);
-
-
+	const char* message="These are the times that try men's souls.";
+	WRITE(new_sockfd, message, strlen(message));
+	close(new_sockfd);
 
 ////////////////////////////////SOCKET END//////////////////////////////////////
 }
@@ -1015,6 +987,7 @@ void client(string client_ip)
   read(sockfd, buffer, 99);
   //READ(sockfd, buffer, 99);
   printf("%s\n", buffer);
+	write(debugFD,buffer,sizeof(buffer));
   close(sockfd);
 
 ////////////////////////////CLIENT SOCKET END///////////////////////////////////
